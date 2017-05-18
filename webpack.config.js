41b90277ebfbe76530953af9602952352e659963
Sean Rose <sean1rose@gmail.com>
2017-05-17 23:46:33 -0700

    mobbin with x

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,23 @@
 var path = require('path');
 var srcPath = path.join(__dirname, 'src');
 var buildPath = path.join(__dirname, 'dist');
+var webpack = require('webpack');
 
 module.exports = {
 	context: srcPath,
-	entry: path.join(srcPath, 'index.js'),
+	entry: [
+		path.join(srcPath, 'index.js')
+	],
 	output: {
 		path: buildPath,
 		filename: "bundle.js"
-	},
-	resolve: {
-		extensions: ['', '.js', '.jsx']
 	},
 	module: {
 		loaders: [
 			{
 				test: /\.jsx?$/,
 				exclude: /(node_modules|bower_components)/,
-				loader: 'babel-loader',
-				query: {
-					presets: ['react', 'es2015']
-				}
+				loader: 'babel-loader'
 			}
 		]
 	}
